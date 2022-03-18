c9596769a03abe37550311edf8727a8c1d96ff9d
ErykWojnarowski <eryk.wojnarowski@microsoft.wsei.edu.pl>
2022-03-18 10:20:32 +0100

    override getlettergrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,24 +13,23 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
                 throw new InvalidOperationException("Trzeba conajmniej 5");
             var prog = (int)Math.Ceiling(Students.Count * 0.2);
-            var oceny = Students.OrderByDescending(a => a.AverageGrade).Select(a => averageGrade).ToList();
+            var oceny = Students.OrderByDescending(a => a.AverageGrade).Select(a => a.AverageGrade).ToList();
             if (averageGrade >= oceny[prog - 1])
             {
                 return 'A';
             }
-            if (averageGrade >= oceny[prog * 2] - 1)
+            if (averageGrade >= oceny[(prog * 2) - 1])
             {
                 return 'B';
             }
-
-            if (averageGrade >= oceny[prog * 3] - 1)
+            if (averageGrade >= oceny[(prog * 3) - 1])
             {
                 return 'C';
             }
-            if (averageGrade >= oceny[prog * 4] -1)
+            if (averageGrade >= oceny[(prog * 4) - 1])
             { 
                 return 'D';
-            }
+            }            
             return 'F';
         }
     }
